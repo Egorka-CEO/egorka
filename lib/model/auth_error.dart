6c5b0404cd29280e2bdfc56e2ee1b0d1747031d7
Hashishvilli <bochko1307@mail.ru>
2023-01-17 01:20:27 +0300

    auth message error

--- lib/model/auth_error.dart
+++ lib/model/auth_error.dart
@@ -1,14 +1,15 @@
 String authError(String str) {
-  switch (str) {
-    case 'Wrong request':
-      return 'Неверный запрос';
-    case 'User not found':
-      return 'Пользователь не найден';
-    case 'Password is wrong':
-      return 'Пароль неверный';
-    case 'Company not found':
-      return 'Компания не найдена';
-    default:
-      return 'Ошибка: $str';
-  }
+  return 'Указаны неверный данные';
+  // switch (str) {
+  //   case 'Wrong request':
+  //     return 'Неверный запрос';
+  //   case 'User not found':
+  //     return 'Пользователь не найден';
+  //   case 'Password is wrong':
+  //     return 'Пароль неверный';
+  //   case 'Company not found':
+  //     return 'Компания не найдена';
+  //   default:
+  //     return 'Ошибка: $str';
+  // }
 }
